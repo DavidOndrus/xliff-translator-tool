27d8001214c2289914b917b1a045cb2c4cfa1e44
David Ondrus <david.ondrus@gmail.com>
2018-01-21 20:53:17 +0100

    Added 'Click' event handlers for menu items

---
+++ Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace XliffTranslatorTool
+{
+    public static class Constants
+    {
+        public const string FILE_DIALOG_DEFAULT_EXT = ".xlf";
+        public const string FILE_DIALOG_FILTER = "XLIFF documents (*.xlf;*.xliff)|*.xlf;*.xliff";
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
 
 namespace XliffTranslatorTool
 {
     public partial class MainWindow : Window
     {
+        private string OpenedFileName { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -14,17 +18,63 @@ namespace XliffTranslatorTool
 
         private void OpenFileMenuOption_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog openFileDialog = CreateOpenFileDialog();
+            bool? result = openFileDialog.ShowDialog();
 
+            string filePath = openFileDialog.FileName;
+            OpenedFileName = openFileDialog.SafeFileName;
+
+            if (result == true)
+            {
+                //send filepath to parser
+            }
         }
 
         private void ImportFileMenuOption_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog openFileDialog = CreateOpenFileDialog();
+            bool? result = openFileDialog.ShowDialog();
 
+            string filePath = openFileDialog.FileName;
+
+            if (result == true)
+            {
+                //send filepath to parser
+            }
         }
 
         private void SaveAsMenuOption_Click(object sender, RoutedEventArgs e)
         {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                FileName = OpenedFileName,
+                DefaultExt = Constants.FILE_DIALOG_DEFAULT_EXT,
+                Filter = Constants.FILE_DIALOG_FILTER,
+                CheckFileExists = true,
+                CheckPathExists = true,
+                OverwritePrompt = true,
+                AddExtension = true
+            };
 
+            bool? result = saveFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog.FileName))
+                {
+                    //write xml to file
+                }
+            }
+        }
+
+        private static OpenFileDialog CreateOpenFileDialog()
+        {
+            return new OpenFileDialog
+            {
+                DefaultExt = Constants.FILE_DIALOG_DEFAULT_EXT,
+                Filter = Constants.FILE_DIALOG_FILTER,
+                Multiselect = false
+            };
         }
     }
 }
