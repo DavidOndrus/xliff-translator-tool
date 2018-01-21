9df789947ba8929288bb2568670fe44d5dfe3411
David Ondrus <david.ondrus@gmail.com>
2018-01-21 22:01:49 +0100

    Added constants needed for XLIFF parsing

--- Constants.cs
+++ Constants.cs
@@ -2,7 +2,29 @@
 {
     public static class Constants
     {
+        #region FILE DIALOGS
         public const string FILE_DIALOG_DEFAULT_EXT = ".xlf";
         public const string FILE_DIALOG_FILTER = "XLIFF documents (*.xlf;*.xliff)|*.xlf;*.xliff";
+        #endregion
+
+        #region XML NODES & ATTRIBUTES
+        public const string XML_NODE_ROOT = "xliff";
+        public const string XML_NODE_FILE = "file";
+        public const string XML_NODE_BODY_V12 = "body";
+        public const string XML_NODE_TRANSLATION_UNIT_V12 = "trans-unit";
+        public const string XML_NODE_TRANSLATION_UNIT_V20 = "unit";
+        public const string XML_NODE_SOURCE = "source";
+        public const string XML_NODE_TARGET = "target";
+        public const string XML_NODE_NOTE = "note";
+        public const string XML_NODE_NOTES_V20 = "notes";
+        public const string XML_NODE_SEGMENT_V20 = "segment";
+        public const string XML_NODE_CONTEXT_GROUP_V12 = "context-group";
+        public const string XML_NODE_CONTEXT_V12 = "context";
+
+        public const string XML_ATTRIBUTE_VERSION = "version";
+        public const string XML_ATTRIBUTE_IDENTIFIER = "id";
+        public const string XML_ATTRIBUTE_EXTRA_DATA_V12 = "from";
+        public const string XML_ATTRIBUTE_EXTRA_DATA_V20 = "category";
+        #endregion
     }
 }
