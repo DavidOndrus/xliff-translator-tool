dcf990a3b35b5aa6816f3423951f9c45b98f52c0
David Ondrus <david.ondrus@gmail.com>
2018-01-21 21:26:06 +0100

    Created data object for DataGrid control

---
+++ Parser/TranslationUnit.cs
@@ -0,0 +1,11 @@
+﻿namespace XliffTranslatorTool.Parser
+{
+    public class TranslationUnit
+    {
+        public string Identifier { get; set; }
+        public string Meaning { get; set; }
+        public string Description { get; set; }
+        public string Source { get; set; }
+        public string Target { get; set; }
+    }
+}
