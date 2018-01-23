61214620fe9bc1480248e664a906e4c1abdf3718
David Ondrus <david.ondrus@gmail.com>
2018-01-23 22:05:50 +0100

    Added application icon

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XliffTranslatorTool")]
+[assembly: AssemblyTitle("Xliff Translator Tool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XliffTranslatorTool")]
+[assembly: AssemblyCompany("David Ondrus")]
+[assembly: AssemblyProduct("Xliff Translator Tool")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
