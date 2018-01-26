2c1e3e057a1ca0523af086e89021ae62c4a8f244
David Ondrus <david.ondrus@gmail.com>
2018-01-26 22:33:26 +0100

    New build (increased version)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Xliff Translator Tool")]
+[assembly: AssemblyTitle("XLIFF Translator Tool")]
 [assembly: AssemblyDescription("Simple XLIFF 1.2 and 2.0 translation tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("David Ondrus")]
-[assembly: AssemblyProduct("Xliff Translator Tool")]
+[assembly: AssemblyProduct("XLIFF Translator Tool")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
